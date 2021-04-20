db71078626121d655664ef3f8d1c6eb6bf4d9d54
Marco Túlio <marcotmotta@gmail.com>
2021-04-20 17:05:11 -0300

    static side generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
-import Header from '../components/Header/Header';
+import { useEffect } from 'react'
 
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
+export default function Home(props) {
+    //Single Page Application method
+    /* useEffect(() => {
+        fetch('http://localhost:3333/episodes')
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }, []) */
+
+    return (
+        <div>{JSON.stringify(props.data)}</div>
+    )
 }
+
+//Server Side Generation method
+export async function getStaticProps() {
+    const response = await fetch('http://localhost:3333/episodes');
+    const data = await response.json();
+
+    return {
+        props: {
+            data: data
+        },
+        revalidate: 60
+    }
+}
+
+//Server Side Rendering method
+//can be accessed by the props of the component
+//must return an object named props
+/* export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3333/episodes');
+    const data = await response.json();
+
+    return {
+        props: {
+            data: data
+        }
+    }
+} */
