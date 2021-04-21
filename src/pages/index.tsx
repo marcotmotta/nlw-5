f7944265a6b8b2518873aa3ec7e994137a730504
Marco Túlio <marcotmotta@gmail.com>
2021-04-21 12:50:00 -0300

    api data factoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,59 @@
-import { useEffect } from 'react'
+import { format, parseISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+import { convertDurationToTime } from '../utils/convertDurationToTime'
 
-export default function Home(props) {
-    //Single Page Application method
-    /* useEffect(() => {
-        fetch('http://localhost:3333/episodes')
-            .then(response => response.json())
-            .then(data => console.log(data));
-    }, []) */
+type Episode = {
+    id: string,
+    title: string,
+    thumbnail: string,
+    members: string,
+    publishedAt: string,
+    duration: string,
+    durationAsString: string,
+    description: string,
+    url: string
+}
 
+type HomeProps = {
+    episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
     return (
-        <div>{JSON.stringify(props.data)}</div>
+        <div>{JSON.stringify(props.episodes)}</div>
     )
 }
 
-//Server Side Generation method
-export async function getStaticProps() {
-    const response = await fetch('http://localhost:3333/episodes');
-    const data = await response.json();
+//Static Side Generation method
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 3,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    });
+
+    const episodes = data.map(episode => {
+        return {
+            id: episode.id,
+            title: episode.title,
+            thumbnail: episode.thumbnail,
+            members: episode.members,
+            publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+            duration: Number(episode.file.duration),
+            durationAsString: convertDurationToTime(Number(episode.file.duration)),
+            description: episode.description,
+            url: episode.file.url
+        };
+    })
 
     return {
         props: {
-            data: data
+            episodes
         },
         revalidate: 60
     }
 }
-
-//Server Side Rendering method
-//can be accessed by the props of the component
-//must return an object named props
-/* export async function getServerSideProps() {
-    const response = await fetch('http://localhost:3333/episodes');
-    const data = await response.json();
-
-    return {
-        props: {
-            data: data
-        }
-    }
-} */
