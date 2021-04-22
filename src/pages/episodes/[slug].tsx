420122b7fed20275861815295af5923301d276e4
Marco Túlio <marcotmotta@gmail.com>
2021-04-22 11:36:53 -0300

    new pges and routing

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,87 @@
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { api } from '../../services/api';
+import { convertDurationToTime } from '../../utils/convertDurationToTime';
+import Image from 'next/image';
+
+import styles from './episode.module.scss';
+
+import { ArrowBack, PlayArrow } from '@material-ui/icons';
+import Link from 'next/link';
+
+type Episode = {
+    id: string,
+    title: string,
+    thumbnail: string,
+    members: string,
+    publishedAt: string,
+    duration: string,
+    durationAsString: string,
+    description: string,
+    url: string
+}
+
+type EpisodeProps = {
+    episode: Episode
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+    return (
+        <div className={styles.episode}>
+            <div className={styles.thumbnail}>
+                <Link href="/">
+                    <ArrowBack className={styles.backBtn} fontSize="large" />
+                </Link>
+                <Image
+                    width={700}
+                    height={160}
+                    src={episode.thumbnail}
+                    objectFit="cover"
+                />
+                <PlayArrow className={styles.playBtn} fontSize="large" />
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div className={styles.description} dangerouslySetInnerHTML={{__html: episode.description}}></div>
+        </div>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { slug } = ctx.params;
+
+    const { data } = await api.get(`episodes/${slug}`);
+
+    const episode = {
+        id: data.id,
+        title: data.title,
+        thumbnail: data.thumbnail,
+        members: data.members,
+        publishedAt: format(parseISO(data.published_at), 'd MMM yy', {locale: ptBR}),
+        duration: Number(data.file.duration),
+        durationAsString: convertDurationToTime(Number(data.file.duration)),
+        description: data.description,
+        url: data.file.url
+    }
+
+    return {
+        props: {
+            episode
+        },
+        revalidate: 60 * 60 * 24 // 24 hours
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,12 @@ import { ptBR } from 'date-fns/locale'
 import { GetStaticProps } from 'next'
 import { api } from '../services/api'
 import { convertDurationToTime } from '../utils/convertDurationToTime'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from './home.module.scss'
+
+import { PlayCircleFilled } from '@material-ui/icons';
 
 type Episode = {
     id: string,
@@ -12,17 +18,95 @@ type Episode = {
     publishedAt: string,
     duration: string,
     durationAsString: string,
-    description: string,
     url: string
 }
 
 type HomeProps = {
-    episodes: Episode[];
+    latestEpisodes: Episode[],
+    allEpisodes: Episode[]
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
     return (
-        <div>{JSON.stringify(props.episodes)}</div>
+        <div className={styles.home}>
+            <section className={styles.latestEpisodes}>
+                <h2>Últimos episódios</h2>
+                <ul>
+                    {latestEpisodes.map(episode => {
+                        return (
+                            <li key={episode.id} className={styles.episode}>
+                                <Image
+                                    width= {192}
+                                    height= {192}
+                                    src={episode.thumbnail}
+                                    alt={episode.title}
+                                    objectFit="cover"
+                                />
+                                <div className={styles.content}>
+                                    <Link href={`/episodes/${episode.id}`}>
+                                        <a>
+                                            <p className={styles.title}>
+                                                {episode.title}
+                                            </p>
+                                        </a>
+                                    </Link>
+                                    <p className={styles.members}>
+                                        {episode.members}
+                                    </p>
+                                    <div className={styles.description}>
+                                        <p className={styles.duration}>
+                                        {episode.publishedAt} | {episode.durationAsString}
+                                        </p>
+                                        <PlayCircleFilled className={styles.playButton} fontSize="large" />
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </section>
+            <section className={styles.allEpisodes}>
+                <h2>Todos os episódios</h2>
+                <table cellSpacing={0}>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Podcast</th>
+                            <th>Integrantes</th>
+                            <th>Data</th>
+                            <th>Duração</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allEpisodes.map(episode => {
+                            return(
+                                 <tr key={episode.id}>
+                                     <td style={{width: 72}}>
+                                         <Image
+                                            width= {120}
+                                            height= {120}
+                                            src={episode.thumbnail}
+                                            alt={episode.title}
+                                            objectFit="cover"
+                                         />
+                                     </td>
+                                     <td>
+                                        <Link href={`/episodes/${episode.id}`}>
+                                            <a>{episode.title}</a>
+                                        </Link>
+                                     </td>
+                                     <td>{episode.members}</td>
+                                     <td style={{width: 100}}>{episode.publishedAt}</td>
+                                     <td>{episode.durationAsString}</td>
+                                     <td><PlayCircleFilled className={styles.playButton} fontSize="large" /></td>
+                                 </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </section>
+        </div>
     )
 }
 
@@ -30,7 +114,7 @@ export default function Home(props: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
     const { data } = await api.get('episodes', {
         params: {
-            _limit: 3,
+            _limit: 12,
             _sort: 'published_at',
             _order: 'desc'
         }
@@ -50,9 +134,13 @@ export const getStaticProps: GetStaticProps = async () => {
         };
     })
 
+    const latestEpisodes = episodes.slice(0, 2);
+    const allEpisodes = episodes.slice(2, episodes.length);
+
     return {
         props: {
-            episodes
+            latestEpisodes,
+            allEpisodes
         },
         revalidate: 60
     }
